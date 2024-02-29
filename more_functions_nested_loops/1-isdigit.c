2b45df4e5552782f359739aeab87b522f123fd8e
Prof-Ursi <159030303+Prof-Ursi@users.noreply.github.com>
2024-02-29 10:44:48 +0100

    Create 1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit using ASCII values (between 48 for '0' and 57 for '9')
+ * @c : variable for check
+ * Return: 1 if is a digit or return 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
