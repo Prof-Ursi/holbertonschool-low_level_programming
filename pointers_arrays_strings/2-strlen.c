a837e442ced6acb250e92330151ef1357415f307
Prof-Ursi <159030303+Prof-Ursi@users.noreply.github.com>
2024-02-29 13:31:10 +0100

    Create 2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s : variable to pointer
+ * Return: i is length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+  
+	return (i);
+}
