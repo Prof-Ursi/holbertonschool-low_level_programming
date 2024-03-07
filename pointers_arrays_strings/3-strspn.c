e5ec16a62f07500c9fbe55f6e36c6a89552deaee
Prof-Ursi <159030303+Prof-Ursi@users.noreply.github.com>
2024-03-07 21:41:48 +0100

    Create 3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * _strspn - search the length of a prefix substring
+  * @s: source string
+  * @accept: string searched
+  *
+  * Return: number of bytes in the init segment
+  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length, i;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		i = 0;
+		while (accept[j] != '\0' && s[length] != accept[i])
+			i++;
+		if (accept[i] == '\0')
+			return (length);
+		length++;
+	}
+	return (length);
+}
