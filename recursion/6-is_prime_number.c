b50ce195434d0e5cf1e05a128c76e005da4b4522
Prof-Ursi <159030303+Prof-Ursi@users.noreply.github.com>
2024-03-08 19:17:37 +0100

    Update 6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
-  * is_prime_number - Returns if a number is prime
-  * @n: the number to be checked
-  *
-  * Return: through prime_checker :
-  * 1 for prime and 0 for composite
-  */
-int is_prime_number(int n)
-{
-	return (prime_checker(n, 1));
-}
-
-/**
   * prime_checker - Check if number is prime
   * @number: the number to be checked
   * @i: counter
@@ -31,4 +19,16 @@ int prime_checker(int number, int i)
 		return (1);
 
 	return (prime_checker(number, i + 1));
+}
+
+/**
+  * is_prime_number - Returns if a number is prime
+  * @n: the number to be checked
+  *
+  * Return: through prime_checker :
+  * 1 for prime and 0 for composite
+  */
+int is_prime_number(int n)
+{
+	return (prime_checker(n, 1));
 }
