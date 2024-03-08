b4e46228ceae465f882453063f8a7a070fdd2271
Prof-Ursi <159030303+Prof-Ursi@users.noreply.github.com>
2024-03-08 19:49:41 +0100

    Create 4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line arguments
+ *
+ * Return:
+ * - 0 if everything worked out
+ * - 1 if the inputs are not digits
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+  int j; 
+  int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
