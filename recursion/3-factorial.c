2581feeca29a7d2247f56bd5db2de5dd81e71acd
Prof-Ursi <159030303+Prof-Ursi@users.noreply.github.com>
2024-03-08 14:01:47 +0100

    Create 3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial
+ * of a given number
+ * @n: number to factorial
+ *
+ * Return: 
+ * - factorial of a given number
+ * - -1 if the number is negative
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
