82fe32ff3226f5f061470b85c57a051f5be0d018
Prof-Ursi <159030303+Prof-Ursi@users.noreply.github.com>
2024-03-08 19:51:56 +0100

    Update 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -14,10 +14,9 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-  	int j;
-  	int sum = 0;
-
+int i;
+int j;
+int sum = 0;
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
