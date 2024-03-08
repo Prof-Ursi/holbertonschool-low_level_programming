c6c6ea93af4ff1f08b683ee3fdcef52c87fb53a6
Prof-Ursi <159030303+Prof-Ursi@users.noreply.github.com>
2024-03-08 19:31:45 +0100

    Update 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  * Return: 0 if everything worked out
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[]) 
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
