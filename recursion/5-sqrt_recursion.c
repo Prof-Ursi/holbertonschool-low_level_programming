291a86d5af44809e37f20376c39311d27e181ad8
Prof-Ursi <159030303+Prof-Ursi@users.noreply.github.com>
2024-03-08 15:59:13 +0100

    Create 5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * comparing_root - checks the input number from n to the base
+ * @root: root is squared and compared against number
+ * @number: number to check
+ * Return:
+ * - natural square root of number number
+ * - -1 if number doesn't have a natural square root
+ */
+
+int comparing_root(int number, int root)
+{
+	if (root * root == number)
+		return (number);
+	if (root * root > number)
+		return (-1);
+	return (comparing_root(number, root++));
+}
+
+/**
+ * _sqrt_recursion - return the natural square root of a number n.
+ * @n: number to check for square roots.
+ * Return: the natural square root of number n
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (comparing_root(n, 0));
+}
