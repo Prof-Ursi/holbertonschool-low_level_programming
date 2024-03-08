7b03e401d4b4c0f17fba8785201c4f60be038046
Prof-Ursi <159030303+Prof-Ursi@users.noreply.github.com>
2024-03-08 15:32:02 +0100

    Update 4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -5,18 +5,18 @@
   * @x: the value to multiply
   * @y: the times to multiply the value x
   *
-  * Return: 
+  * Return:
   * - the value x multiplied y times
   * - -1 if y is lower than 0
   */
 
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-        return (-1);
+	if (y < 0)
+		return (-1);
 
-    if (y == 0)
-        return (1);
+	if (y == 0)
+		return (1);
 
-    return (x * _pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
