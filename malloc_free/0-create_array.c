1a1c3638db40a97cfd618c000610f6a1511ba2fa
Prof-Ursi <159030303+Prof-Ursi@users.noreply.github.com>
2024-03-14 13:50:02 +0100

    Update 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,15 +12,16 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
+	unsigned int i;
 
 	array = malloc(sizeof(char) * size);
 
-	if (!size || array == NULL)
+	if (size == 0 || array == NULL)
 	{
 		return (NULL);
 	}
 
-	for (int i = 0; i < size; ++i)
+	for (i = 0; i < size; ++i)
 	{
 		array[i] = c;
 	}
