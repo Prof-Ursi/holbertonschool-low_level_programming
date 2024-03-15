e5d4441c6ff0fac0687f9ba8062657ea13003b17
Prof-Ursi <8743@holbertonstudents.com>
2024-03-15 15:34:51 +0100

    fix: updating condition

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,20 +10,18 @@
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		if ((*d).name == NULL)
-			printf("(nil)");
-		else
-			printf("Name: %s\n", (*d).name);
-
-		printf("Age: %f\n", (*d).age);
-
-		if ((*d).owner == NULL)
-			printf("(nil)");
-		else
-			printf("Owner: %s\n", (*d).owner);
-	}
-	else
+	if (d == NULL)
 		return;
+
+	if ((*d).name == NULL)
+		printf("(nil)");
+	else
+		printf("Name: %s\n", (*d).name);
+
+	printf("Age: %f\n", (*d).age);
+
+	if ((*d).owner == NULL)
+		printf("(nil)");
+	else
+		printf("Owner: %s\n", (*d).owner);
 }
