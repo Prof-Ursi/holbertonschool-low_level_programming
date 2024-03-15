a10fd4aa592d39480b1daf59bed63f3e634441ce
Prof-Ursi <8743@holbertonstudents.com>
2024-03-15 15:28:05 +0100

    fix: updating output with (Nil)

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,17 +13,14 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if ((*d).name == NULL)
-			printf("Name: (nil)");
+			printf("(nil)");
 		else
 			printf("Name: %s\n", (*d).name);
 
-		if ((*d).age < 0)
-			printf("Age: (nil)\n");
-		else
-			printf("Age: %f\n", (*d).age);
+		printf("Age: %f\n", (*d).age);
 
 		if ((*d).owner == NULL)
-			printf("Owner: (nil)");
+			printf("(nil)");
 		else
 			printf("Owner: %s\n", (*d).owner);
 	}
