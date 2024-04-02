64e09d844088e7a71c40cff05f6505e5e62c6fd9
Prof-Ursi <8743@holbertonstudents.com>
2024-04-02 13:34:06 +0200

    Adding 2-add_node.c to Repo, working locally & betty approved

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node at the beginning of a list
+ *
+ * @head: pointer to the head of the list
+ * @str: string to add to the new node
+ *
+ * Return: pointer to the new head of the list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL || head == NULL)
+		free(new);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
