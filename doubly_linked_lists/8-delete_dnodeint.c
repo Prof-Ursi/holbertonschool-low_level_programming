fb7f8a3ac1948b195297761f326874f9c81911d9
Prof-Ursi <8743@holbertonstudents.com>
2024-04-04 11:54:53 +0200

    Updating 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -27,11 +27,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	while (i < index)
 	{
-		if (current->next == NULL)
-			return (-1);
 		current = current->next;
+		i++
 	}
-	current->prev->next = current->next;
+	if (current == NULL)
+		return (-1);
+	if (current->prev != NULL)
+		current->prev->next = current->next;
 	if (current->next != NULL)
 		current->next->prev = current->prev;
 	free(current);
