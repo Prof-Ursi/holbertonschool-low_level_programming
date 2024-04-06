5ddd3718e51a245192f4118b1eb97bb950d6f19e
Prof-Ursi <8743@holbertonstudents.com>
2024-04-06 12:49:55 +0200

    Fix : correct typo error in function name

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@
  * Return: 1 if it worked, or -1 if an error occurred
  */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(unsigned long int) * 8)
 	{
