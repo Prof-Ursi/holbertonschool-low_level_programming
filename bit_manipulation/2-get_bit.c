fe3f1b5e40108a10dde17f6bcadffc8b4cd38963
Prof-Ursi <8743@holbertonstudents.com>
2024-04-06 12:31:03 +0200

    Updating 2-get_bit.c, adding bit witchcraft comments

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -4,7 +4,8 @@
  * get_bit - return the value of a bit at a given index
  *
  * @n: the number to retrieve the bit from
- * @index: the index of the bit to retrieve inside n
+ * @index: the index of the bit to retrieve inside n, starting from the right
+ * at index = 0
  *
  * Return: the value of the bit at the given index or -1 if an error occurred
  */
@@ -16,4 +17,11 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (-1);
 	}
 	return ((n >> index) & 1);
+	/*BIT WITCHCRAFT :
+	First we right shift the number with index value,
+	setting the rightmost bit of the number as the one we want to retrieve,
+	then we compare this rightmost bit to 1 with bitwise AND
+	to set every bit on the left as 0, thus returning only
+	1 if the index value is 1 and 0 otherwise.*/
+
 }
