563c2002061aa3115c25dd669f4040e6e4163865
Prof-Ursi <8743@holbertonstudents.com>
2024-04-06 12:29:11 +0200

    Updating 1-print_binary.c, adding bit witchcraft comments

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -5,7 +5,7 @@
  * - arrays not allowed
  * - malloc not allowed
  * - % and / not allowed
- * => recursion used
+ * => recursion time
  *
  * @n: integer number to be transformed into binary
  */
@@ -14,6 +14,17 @@ void print_binary(unsigned long int n)
 {
 
 	if (n > 1)
-		print_binary(n >> 1);
-	_putchar('0' + (n & 1));
+		print_binary(n >> 1); /*Recursion case*/
+		/*BIT WITCHCRAFT # 1:
+		As long as n > 1, we go inside the recursion and shift the number n by
+		1 place to the right until we reach the leftmost 1 in n,
+		thus reaching the base case.*/
+	_putchar('0' + (n & 1)); /*Base case*/
+	/*BIT WITCHCRAFT # 2:
+	First we fetch the rightmost bit of the current n 
+	by comparing it to 1 with bitwise AND,
+	thus setting every bit to its left to zero and fetching either 0 or 1,
+	then we set '0' as our ASCII Code reference to feed to _putchar,
+	and then add to that the rightmost bit of n to send either '0' or '1' to
+	standard output*/
 }
